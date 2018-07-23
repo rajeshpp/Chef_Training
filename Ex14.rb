6f1299037726c6cb2ba936d67ad27deb7be15ec8
rajeshpp <rajesh.paleru@gmail.com>
2018-07-23 14:49:50 +0530

    Update Ex14.rb
    some more functions added

--- Ex14.rb
+++ Ex14.rb
@@ -15,7 +15,23 @@ def print_none()
     puts "I've got nothing"
 end
 
+def cheese_and_crackers(cheese_count,boxes_of_crackers)
+    puts "You have #{cheese_count} cheeses!"
+    puts "You have #{boxes_of_crackers} boxes of crackers!"
+    puts "Men that's enough for a party!"
+    puts "Get a blanket.\n"
+end
+
 print_two("Raj","esh")
 print_two_again("Raj","esh")
 print_one("First and only argument!")
 print_none()
+
+puts "We can just give function numbers directly"
+cheese_and_crackers(20,30)
+
+puts "Or we can use variables from our script:"
+amount_of_cheese=10
+amount_of_crackers=50
+
+cheese_and_crackers(amount_of_cheese,amount_of_crackers)
