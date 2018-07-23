aef7ec2915c822c50d9c5001e86e315138e6d4dd
rajeshpp <rajesh.paleru@gmail.com>
2018-07-23 11:40:24 +0530

    Create Ex6.rb
    Special variables

---
+++ Ex6.rb
@@ -0,0 +1,15 @@
+tabby_cat="\tI'm tabbed in"
+persian_cat="I'm split\non a line"
+back_slash="I'm \\ a \\ cat"
+
+fat_cat="""
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts back_slash
+puts fat_cat
