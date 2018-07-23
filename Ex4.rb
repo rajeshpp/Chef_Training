a161f67c374b8a9de5f070544c04f657e9399d9d
rajeshpp <rajesh.paleru@gmail.com>
2018-07-23 11:14:41 +0530

    Update Ex4.rb
    variables 2

--- Ex4.rb
+++ Ex4.rb
@@ -1,6 +1,11 @@
 my_name='Rajesh'
 my_age=32
+my_eyes='Blue'
+my_hair='Brown'
+my_teeth='White'
 
 puts "Let's talk about me:"
 puts "My name is: #{my_name}"
 puts "My age is: #{my_age}"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee"  
